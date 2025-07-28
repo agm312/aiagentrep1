d31b0262ca66220dbacadae00052e2ccf76879c0
agm312 <agmart17@gmail.com>
2025-07-28 15:14:36 -0500

    Fix setup cost page layout and remove unused image preload causing console warnings

--- src/pages/SetupCostForAIAutomation.js
+++ src/pages/SetupCostForAIAutomation.js
@@ -54,7 +54,7 @@ const SetupCostForAIAutomation = () => {
         </script>
       </Helmet>
       
-      <main className="bg-white">
+      <div className="bg-white min-h-screen">
         {/* Hero Section */}
         <section className="bg-gradient-to-br from-blue-50 to-indigo-100 py-16 px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto text-center">
@@ -550,7 +550,7 @@ const SetupCostForAIAutomation = () => {
             </div>
           </div>
         </section>
-      </main>
+      </div>
     </>
   );
 };
